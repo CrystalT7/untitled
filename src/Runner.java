09035115265ec29170ca3e6fa25e56ff2e4465ea
BT_1N3_24 <sample@bths.edu>
2024-10-25 10:02:51 -0400

    added x value and changed the hello print statement

--- src/Runner.java
+++ src/Runner.java
@@ -1,5 +1,8 @@
 public class Runner {
     public static void main(String[] args) {
-        System.out.println("hello");
+        System.out.println("hel1lo");
+        int x = 13;
+        x ++;
+        System.out.println(x);
     }
 }
