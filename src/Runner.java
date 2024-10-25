ae74946ef8598e3cbde267084c9270b8b8adc419
BT_1N3_24 <sample@bths.edu>
2024-10-25 10:04:25 -0400

    added x value and changed the hello print statement

--- src/Runner.java
+++ src/Runner.java
@@ -4,5 +4,8 @@ public class Runner {
         int x = 13;
         x ++;
         System.out.println(x);
+        System.out.println("i love taylor swift");
+        int y = 12;
+        System.out.println("Her birth month is december");
     }
 }
